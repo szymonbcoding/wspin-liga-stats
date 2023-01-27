e5ea4acf293856c12a5cdf980d4cad8e63b0b1f5
sbalawajder <sbalawajder@man.poznan.pl>
2023-01-27 12:33:46 +0100

    Added function download_files to main.py Downloaded all minus 2 html files from firebase

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import requests
 import logging
 import os
 from dotenv import load_dotenv
-from apscheduler.schedulers.blocking import BlockingScheduler
+# from apscheduler.schedulers.blocking import BlockingScheduler
 import time
 
 import pyrebase
 
-load_dotenv()
+# load_dotenv()
 
 FIREBASE_CONFIG = {
   "apiKey": "AIzaSyDg8-HLK6LmbChoGyLbN5Sga1NwjjzFMKA",
@@ -63,16 +63,56 @@ def some_job(config) -> None:
     put_file_to_firebase(config, 
                          path_on_cloud=file_name,
                          path_local=file_name)
+def download_files(filenames: list, config: dict[str, str]):
+    firebase = pyrebase.initialize_app(config)
+
+    storage = firebase.storage()
+
+    for filename in filenames:
+        data = storage.child(f"data/{filename}").download(filename = f"data/html/{filename}", path=f"data/{filename}")
 
 if __name__ == '__main__':
     
-    firebase_config = configure_firebase()
+    firebase_config = FIREBASE_CONFIG
     
-    scheduler = BlockingScheduler()
-    scheduler.add_job(some_job, 'interval', minutes = 5)
-    scheduler.start()
+    # scheduler = BlockingScheduler()
+    # scheduler.add_job(some_job, 'interval', minutes = 5)
+    # scheduler.start()
     
-    # do job once a 5 minutes
-    some_job(config = firebase_config)
+    # # do job once a 5 minutes
+    # some_job(config = firebase_config)
+
+    
+
+    filenames = [
+        "20230104-143204.html",
+        "20230105-143203.html",
+        "20230106-143205.html",
+        "20230107-143203.html",
+        "20230108-143204.html",
+        "20230109-143203.html",
+        "20230110-143204.html",
+        "20230111-143203.html",
+        "20230112-144203.html",
+        "20230113-143205.html",
+        "20230114-143206.html",
+        "20230115-143203.html",
+        "20230116-143203.html",
+        "20230117-143203.html",
+        "20230118-143205.html",
+        "20230119-143206.html",
+        "20230120-143203.html",
+        "20230121-143206.html",
+        "20230122-143204.html",
+        "20230123-143206.html",
+        "20230124-143203.html",
+        "20230125-143204.html",
+        "20230126-143203.html",
+        # 27
+        # 28
+    ]
+    download_files(filenames, firebase_config)
+    
+
 
     
