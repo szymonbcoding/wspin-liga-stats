c70f9309226bb25fc645b03e2bb73d88eb036409
sbalawajder <sbalawajder@man.poznan.pl>
2023-01-29 13:55:46 +0100

    Create function for generate_bcr Added example mp4 result

--- generate_bar_chart_race.py
+++ generate_bar_chart_race.py
@@ -1,10 +1,23 @@
 import bar_chart_race as bcr
 import pandas as pd
 
-df = pd.read_csv("data/main_csv/m.csv", index_col='time')
-bcr.bar_chart_race(
+def generate_bcr(male: bool):
+    if(male):
+        df = pd.read_csv("data/main_csv/m.csv", index_col='time')
+        title = 'Noworoczna Liga Wspinaczkowa - Mężyczyźni (autor: Szymon Balawajder)'
+        filename = 'wspinliga_male.mp4'
+    else:
+        df = pd.read_csv("data/main_csv/f.csv", index_col='time')
+        title = 'Noworoczna Liga Wspinaczkowa - Kobiety (autor: Szymon Balawajder)'
+        filename = 'wspinliga_female.mp4'
+
+    bcr.bar_chart_race(
     df=df,
-    filename = 'wspinliga_male.mp4',
+    filename = filename,
     n_bars=16,
-    title = 'Noworoczna Liga Wspinaczkowa Mężyczyźni'
+    period_length=125,
+    title = title
 )
+
+if __name__ == '__main__':
+    generate_bcr(False)
