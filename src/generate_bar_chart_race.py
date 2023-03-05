e11a00fd323af7f1c349f3a2e981b6283d9ca64b
szymonbcoding <szymonbalawajder@gmail.com>
2023-03-05 14:05:10 +0100

    Removed mp4 files

--- src/generate_bar_chart_race.py
+++ src/generate_bar_chart_race.py
@@ -5,11 +5,11 @@ def generate_bcr(male: bool):
     if(male):
         df = pd.read_csv("data/main_csv/m.csv", index_col='time')
         title = 'Noworoczna Liga Wspinaczkowa - Mężyczyźni'
-        filename = 'wspinliga_male.mp4'
+        filename = 'data/mp4/wspinliga_male.mp4'
     else:
         df = pd.read_csv("data/main_csv/f.csv", index_col='time')
         title = 'Noworoczna Liga Wspinaczkowa - Kobiety '
-        filename = 'wspinliga_female.mp4'
+        filename = 'data/mp4/wspinliga_female.mp4'
 
     bcr.bar_chart_race(
     df=df,
